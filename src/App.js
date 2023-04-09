ba229ecffc163903622ef37a75517b93962f6f2e
linhvth <vuthihonglinh.work@gmail.com>
2023-04-09 13:25:54 +0700

    Add Github Actions

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test the initial setup for Meal Plan Website project from the course ENG 301.
         </p>
         <a
           className="App-link"
