097642e912803775b9367ac07d4b77740d37fc90
chikim0101 <hgkchi124@gmail.com>
2023-04-22 17:57:51 +0700

    modify

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,31 @@
 import React from "react";
 import { Container, Card } from "react-bootstrap";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import './Home.scss'
 
 import OurPlans from "../elements/OurPlans";
 import OurDishes from "../elements/OurDish";
 import Articles from "../elements/Articles";
 
-import background from '../images/slide.jpg';
+import background from '../images/home_background.jpg';
 
 function Home() {
+    const navigation = useNavigate();
+    const navigateTo = () => navigation('../components/LogIn');
 
     return (
         <Container fluid className="p-0">
             <Card className="home_background">
-                <img class ="background" src = { background } alt = { background } responsive/>
+                <img className ="background" src = { background } alt = { background } responsive/>
                 <h1 className="background_text">
                     Customize your diet with selection from thousands of dishes <br/>
-                    Meal planning has never been easier with Good Meals!
+                    <div className = "spacing-40"></div>
+                    Meal planning has never been easier with <strong>Good Meals!</strong>
+                    <div className='division'>
+                        <button className='still' onClick = {navigateTo}>
+                        Sign Up Now                
+                        </button>          
+                    </div>
                 </h1>
             </Card>
             <OurPlans/>
