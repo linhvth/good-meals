ac822adc1e6fd282ade65cd4b1a83d4d449d4c39
linhvth <vuthihonglinh.work@gmail.com>
2023-05-16 12:17:47 +0700

    setup query data

--- src/components/SingleRecipe.js
+++ src/components/SingleRecipe.js
@@ -2,56 +2,15 @@ import React, { useState, useEffect } from "react";
 import axios from "axios"
 import { useParams } from "react-router-dom";
 
-const SingleRecipe = () =>{
-  let { slug } = useParams()
-  const qs = require('qs');
-  const query = qs.stringify({
-    filters: {
-      slug: {
-        $eq: slug,
-      },
-    },
-    populate: '*',
-  }, {
-    encodeValuesOnly: true,
-  });
+import { db } from '../firebase';
+import { Container } from "react-bootstrap";
 
-  const [event, setEvent] = useState([])
-  useEffect(() => {
-    const fetchEvent = async () => {
-      const res = await axios.get(`${BACKEND_URL}/api/events?${query}`)
-      setEvent(res.data.data)
-    }
-
-    fetchEvent()
-  }, [])
-
-
-  const FeatureImg = (imgsrc) => {
-    const image = imgsrc.imgsrc.data
-      ? imgsrc.imgsrc.data.attributes.url
-      : dummy;
-    
-    return (
-      <img src={image} width="100%" className="my-4"/>
-    )
-  }
+const SingleRecipe = () => {
 
   return(
-    <div className="pt-4 container col-lg-7">
-      {event
-      .map(({attributes}) =>
-        <div>
-          <SectionHeader text={ attributes.name } className="mt-5"/>
-          <FeatureImg imgsrc={ attributes.featureImage }/>
-          <ReactMarkdown className="my-4" components={{img:({src, ...props}) =>{
-            console.log(src);
-            return <img src={src} width="100%" className="my-4"/> }}}>
-            {attributes.description}
-          </ReactMarkdown>
-        </div>
-      )}
-    </div>
+    <Container>
+        Coming soon.
+    </Container>
   )
 
 }
