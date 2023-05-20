e312cab07cf00952a948671763f8027dbb4e20e2
imkhoibui <khoi.bui.200153@student.fulbright.edu.vn>
2023-05-20 10:02:35 +0700

    Added My Diet

--- src/elements/MyAccount.js
+++ src/elements/MyAccount.js
@@ -1,46 +1,23 @@
-import { useNavigate } from 'react-router-dom';
-import { Nav, Navbar, Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { Routes, Route, useLocation } from 'react-router-dom';
 
-import GeneralInfo from './accountpages/GeneralInfo';
 import Settings from './accountpages/Settings';
 import MyDiet from './accountpages/MyDiet';
 import MyPlan from './accountpages/MyPlan';
 import Bookmarks from './accountpages/Bookmarks';
+import Sidebar from './accountpages/Sidebar';
 
 export function PageToggle() {
   return (
     <Routes>
       <Route path="/">
-        <Route index element={ <GeneralInfo/> } />
-        <Route path="my-account/settings" element={ <Settings/> } />
-        <Route path="my-account/my-diet" element={ <MyDiet/> } />
-        <Route path="my-account/my-plan" element={ <MyPlan/> } />
-        <Route path="my-account/bookmarks" element={ <Bookmarks/> } />
+        <Route index element={ <Settings/> } />
+        <Route path="my-diet" element={ <MyDiet/> } />
+        <Route path="my-plan" element={ <MyPlan/> } />
+        <Route path="bookmarks" element={ <Bookmarks/> } />
       </Route>
     </Routes>
   )
-}
-
-export function Sidebar() {
-  const navigate = useNavigate();
-
-  return (
-    <Navbar expand="lg" id='profile-setting'>
-      <Container className = "setting-box">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="flex-column me-auto w-100">
-            <Nav.Link onClick={() => navigate("/my-account")}>General Info</Nav.Link>
-            <Nav.Link onClick={() => navigate("/my-account/settings")}>Settings</Nav.Link>
-            <Nav.Link onClick={() => navigate("/my-account/my-diet")}>My Diet</Nav.Link>
-            <Nav.Link onClick={() => navigate("/my-account/my-plan")}>My Plan</Nav.Link>
-            <Nav.Link onClick={() => navigate("/my-account/bookmarks")}>Bookmarks</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
 }
 
 const MyAccount = () => {
@@ -62,6 +39,5 @@ const MyAccount = () => {
     </Container>
   )
 }
-
 
 export default MyAccount
--- src/elements/accountpages/Bookmarks.js
+++ src/elements/accountpages/Bookmarks.js
@@ -1,3 +1,72 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { Button, Container, Row, Col, Form } from 'react-bootstrap';
+import { db } from '../../firebase';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+import './MyDiet.scss'
+
+const auth = getAuth();
+
+export function DietContent() {
+  const [ docRef, setDocRef ] = useState(null);
+  const [ savedInfo, setSavedInfo ] = useState({
+    bookmarks: []
+  })
+
+  const getData = useCallback(async () => {
+    if (docRef) {
+      const data = await getDoc(docRef);
+      if (data.exists()) {
+        setSavedInfo({ ...data.data(), id: data.id });
+      }
+    }
+  }, [docRef]);
+  
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setSavedInfo((prevSavedInfo) => ({
+      ...prevSavedInfo,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // Update the userInfo document in the database
+    await updateDoc(docRef, savedInfo);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const curruid = user.uid;
+        setDocRef(doc(db, 'userInfo', curruid));
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    getData();
+  }, [docRef, getData])
+
+  const measurements = [
+    { label: 'Saved Dishes', name: "savedDish", value: savedInfo.savedDish },
+  ]
+
+
+  return (
+    <Container>
+        <h2 className="section-head">Bookmarked Dishes</h2>
+
+
+
+    </Container>
+  );
+}
+
 const Bookmarks = () => {
 
 }
